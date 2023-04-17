bbdeecbc8690ed28edd6026e6e2c6e870ade364d
warpakner123 <130353421+warpakner123@users.noreply.github.com>
2023-04-17 23:38:36 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import { StyleSheet, useState } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Login from './screens/login';
@@ -18,12 +17,3 @@ export default function App() {
   </NavigationContainer>
   );
 }
-
-/*const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});**/
