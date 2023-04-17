2709f3715f3d7abf3f29c3b689b89059894cc218
warpakner123 <130353421+warpakner123@users.noreply.github.com>
2023-04-17 16:25:33 +0800

    Update axios.js

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-axios.defaults.baseURL = 'https://api.example.com'; // url sa localhost 
-axios.defaults.headers.post['Content-Type'] = 'application/json';
+axios.defaults.baseURL='http://192.168.0.174:8000/api/v1/' // url sa localhost  // mag vary ni sa end sa user
+axios.defaults.headers.post['Content-Type'] = 'application/json'
 
 export default axios;
