d52467c8e30ff0487ffc7d0a9aa3968823ec4cb0
warpakner123 <130353421+warpakner123@users.noreply.github.com>
2023-04-17 16:29:28 +0800

    Update index.js
    Dili maka retrieve ug data from API

--- screens/login/index.js
+++ screens/login/index.js
@@ -1,65 +1,84 @@
-import React, { useState } from "react";
-import { StyleSheet, TextInput, View, Button, Alert } from "react-native";
-import axios from "../../plugins/axios";
-import InputField from "../../components/InputField";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+// import axios from "../../plugins/axios";
 import AsyncStorage from "@react-native-async-storage/async-storage"; // npm install @react-native-async-storage/async-storage // dili mag run saimuhang end
 
-const Login = ({ navigation }) => {
-  const [data, setData] = useState({
-    email: "",
-    password: "",
-  });
+const Profile = () => {
+  const [userData, setUserData] = useState(null);
+  const [error, setError] = useState(null); // added error state
+  const [token, setToken] = useState(null); // added token state
 
-  const handleRegistration = () => {
-    navigation.navigate('Register');
-  };
-
-  const handleLogin = () => {
-    axios
-      .post("accounts/token/login/", data)
-      .then(async (response) => {
-        await AsyncStorage.setItem('token', response.data.auth_token);
-        console.log(response);
-        Alert.alert("Login successful!");
-        navigation.navigate('Profile'); // navigate to profile on success
-      })
-      .catch((error) => {
-        if (error.response && error.response.data) {
-          console.log(error.response.data);
-          Alert.alert("Login failed.");
+  useEffect(() => {
+    // Retrieve user data from server
+    const fetchUserData = async () => {
+      try {
+        const storedToken = await AsyncStorage.getItem("token");
+        setToken(storedToken);
+        const response = await fetch("http://127.0.0.1:8000/api/v1/accounts/users/", {
+          headers: {
+            Authorization: `Token ${storedToken}`,
+          },
+        });
+        if (response.status === 401) {
+          // Handle 401 unauthorized error
+          setError("Unauthorized. Please login again.");
         } else {
-          console.log(error);
-          Alert.alert("An error occurred.");
+          const userData = await response.json();
+          setUserData(userData);
         }
-      });
-  };
+      } catch (error) {
+        // Handle other errors
+        setError("Failed to retrieve user data.");
+        console.log(error);
+      }
+    };
+
+    fetchUserData();
+  }, []);
+
+  if (error) {
+    // Display error message if error state is set
+    return (
+      <View style={styles.container}>
+        <Text style={styles.error}>{error}</Text>
+        <Button
+          title="Go to Login"
+          onPress={() => {
+            // Navigate to login screen on error
+            setError(null); // Reset error state
+            // You can replace this with your actual login navigation logic
+            // e.g. navigation.navigate('Login');
+          }}
+        />
+      </View>
+    );
+  }
 
   return (
-    <View style={styles.container}>
-      <InputField
-        label="Email"
-        placeholder="Your Email"
-        value={data.email}
-        onChangeText={(email) => setData({ ...data, email })}
-      />
-      <InputField
-        label="Password"
-        placeholder="Your Password"
-        value={data.password}
-        onChangeText={(password) => setData({ ...data, password })}
-        secureTextEntry
-      />
-      <Button title="Submit" onPress={handleLogin} />
-      <Button title="Register" onPress={handleRegistration} />
-    </View>
+    <>
+      <View style={styles.container}>
+        {/* Render user data */}
+        <Text style={styles.label}>Email:</Text>
+        <Text style={styles.value}>{userData?.email}</Text>
+      </View>
+    </>
   );
 };
 
-export default Login;
+export default Profile;
 
 const styles = StyleSheet.create({
   container: {
     paddingHorizontal: 20,
     paddingVertical: 30,
+  },
+  label: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  value: {
+    fontSize: 16,
+    marginBottom: 15,
   },
 });
